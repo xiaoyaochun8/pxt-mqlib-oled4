e2384a280b551e5b763b6f501c14c8a7ae5f42b3
cblvip <wjyzxhm888@163.com>
2025-12-11 10:38:38 +0800

    1

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
+/**
+ * mqOled blocks
+ */
+//% groups=['oled-面板库']
+namespace mqlib{
 
+    let screen1024 = pins.createBuffer(1024);
+
+    //% subcategory="oled"
+    //% group='oled-面板库'
+    //% block
+    function drawPanel(x = 1, y = 1):void {
+        screen1024.fill(0);
+        for(var i=0;i<127;i++){
+            OLED12864_I2C.pixelM(i, 0);
+            OLED12864_I2C.pixelM(i, 63);
+        }
+        for(var i=0;i<64;i++){
+            OLED12864_I2C.pixelM(0, i);
+            OLED12864_I2C.pixelM(127, i);
+        }
+        OLED12864_I2C.showString(1, 1, 'X='+x);
+        OLED12864_I2C.showString(1, 2, 'Y='+y);
+        OLED12864_I2C.draw();
+    }
+
+}
