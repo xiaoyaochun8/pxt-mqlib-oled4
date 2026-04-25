0f8b7af0e33a3e80ea861a88b8ea12e3ba14af99
Barry.C <wjyzxhm888@163.com>
2026-04-25 10:18:53 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -9,13 +9,13 @@ namespace mqlib{
     //% subcategory="oled"
     //% group='oled-面板库'
     //% block="oled面板 x=$x y=$y"
-    function oledDrawPanel(x = 1, y = 1):void {
+    export function oledDrawPanel(x = 1, y = 1):void {
         screen1024.fill(0);
-        for(var i=0;i<127;i++){
+        for(let i=0;i<127;i++){
             OLED12864_I2C.pixel(i, 0);
             OLED12864_I2C.pixel(i, 63);
         }
-        for(var i=0;i<64;i++){
+        for(let i=0;i<64;i++){
             OLED12864_I2C.pixel(0, i);
             OLED12864_I2C.pixel(127, i);
         }
